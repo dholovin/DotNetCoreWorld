28edfd6bd15793151895f24e36459cd66de6059c
denys.holovin <denys.holovin@cgi.com>
2017-09-22 17:33:27 -0300

    model introduced

---
+++ World/Models/Stop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace World.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Order { get; set; }
+        public DateTime Arrival { get; set; }        
+    }
+
+
+}
---
+++ World/Models/Trip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace World.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<Stop> Stops { get; set; }
+    }
+}
